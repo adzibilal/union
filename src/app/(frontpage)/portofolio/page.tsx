e4c3ec87b34972292a5a66733a2df8ddcf363afc
adzibilal <adzibilal02@gmail.com>
2024-06-13 17:30:28 +0700

    porto page

--- src/app/(frontpage)/portofolio/page.tsx
+++ src/app/(frontpage)/portofolio/page.tsx
@@ -1,4 +1,6 @@
 import PageHeader from '@/components/organism/PageHeader'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const PortofolioPage = () => {
@@ -8,6 +10,36 @@ const PortofolioPage = () => {
                 title='Portofolio'
                 subtitle='List of our work that we have done.'
             />
+
+            <div className='max-container py-10'>
+                <div className='grid grid-cols-3 gap-3'>
+                    <div className='shadow-lg'>
+                        <Image
+                            src='/assets/images/porto/1.png'
+                            alt='about'
+                            width={500}
+                            height={600}
+                            className='aspect-[5/4] w-full object-cover'
+                        />
+                        <div className='p-3'>
+                            <div className=''>PRE MADE HOME CONSULTATION</div>
+                            <div className='grid grid-cols-2'>
+                                <div className='text-sm text-zinc-500'>
+                                    Interior Design
+                                </div>
+                                <div className='text-sm text-zinc-500'>
+                                    Construction
+                                </div>
+                            </div>
+                            <Link href={`#`} target='_blank'>
+                                <div className='bg-u-orange-500 mt-5 text-white px-6 py-3 w-max hover:bg-u-orange-300 cursor-pointer'>
+                                    Selengkapnya
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/organism/PageHeader.tsx
+++ src/components/organism/PageHeader.tsx
@@ -7,7 +7,7 @@ interface PageHeaderProps {
 
 const PageHeader: React.FC<PageHeaderProps> = ({ title, subtitle }) => {
     return (
-        <div className='w-full h-[30vh] max-sm:h-max max-sm:py-10 bg-u-orange-500 text-white'>
+        <div className='w-full h-[20vh] max-sm:h-max max-sm:py-10 bg-u-orange-500 text-white'>
             <div className='max-container w-full h-full flex justify-center flex-col'>
                 <div className='uppercase font-extrabold text-6xl max-sm:text-2xl'>
                     {title}
